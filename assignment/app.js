0a9e9c58d894f3e69da1682e54a3df8c17700157
lexylena <lexylena@gmail.com>
2017-06-09 17:04:48 -0400

    db setup

--- assignment/app.js
+++ assignment/app.js
@@ -7,3 +7,7 @@ require('./services/user.service.server');
 require('./services/website.service.server');
 require('./services/page.service.server');
 require('./services/widget.service.server');
+
+var mongoose = require('mongoose');
+mongoose.Promise = require('q').Promise;
+mongoose.connect('mongodb://localhost/webdev');
