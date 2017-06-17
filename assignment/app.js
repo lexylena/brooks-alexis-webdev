22a07b0385b2331d2d260129c0bd604a93bd8b7c
lexylena <lexylena@gmail.com>
2017-06-17 08:07:22 -0400

    fix db setup

--- assignment/app.js
+++ assignment/app.js
@@ -10,3 +10,13 @@ require('./services/widget.service.server');
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('q').Promise;
+
+var connectionString = 'mongodb://127.0.0.1:27017/test';
+if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
+    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds157349.mlab.com:57349/heroku_cjw12ssz';
+}
+
+mongoose.connect(connectionString);
