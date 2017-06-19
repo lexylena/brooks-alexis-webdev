414cf293dacf4942779b4e09b6a5cf35804fd419
lexylena <lexylena@gmail.com>
2017-06-18 20:40:30 -0400

    clean up console log

--- public/assignment/views/widget/controllers/flickr-image-search.controller.client.js
+++ public/assignment/views/widget/controllers/flickr-image-search.controller.client.js
@@ -38,11 +38,9 @@
         }
 
         function searchPhotos(searchTerm) {
-            console.log(searchTerm);
             flickrService
                 .searchPhotos(searchTerm)
                 .then(function(response) {
-                    console.log(response.data);
                     data = response.data.replace("jsonFlickrApi(","");
                     data = data.substring(0,data.length - 1);
                     data = JSON.parse(data);
