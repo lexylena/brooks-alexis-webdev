676b74a61a4a35d8842d3a9b1004e2ea805c1bfd
lexylena <lexylena@gmail.com>
2017-06-21 23:06:24 -0400

    basic comment client service

--- public/project/services/comment.service.client.js
+++ public/project/services/comment.service.client.js
@@ -8,5 +8,46 @@
 
     function commentService($http) {
 
+        var baseUrl = '/api/project/comment';
+
+        return {
+            addComment: addComment,
+            findCommentById: findCommentById,
+            findCommentsForSelection: findCommentsForSelection,
+            deleteComment: deleteComment
+        //    update?
+        };
+
+        function addComment(selectionId, comment) {
+            comment._selection = selectionId;
+            return $http.post(baseUrl, comment)
+                .then(function (response) {
+                    return response.data;
+                })
+        }
+
+        function findCommentById(commentId) {
+            var url = baseUrl + '/' + commentId;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                })
+        }
+
+        function findCommentsForSelection(selectionId) {
+            var url = baseUrl + '?selection=' + selectionId;
+            return $http.get(url)
+                .then(function (response) {
+                    return response.data;
+                })
+        }
+
+        function deleteComment(commentId) {
+            var url = baseUrl + '/' + commentId;
+            return $http.delete(url)
+                .then(function (response) {
+                    return response.data;
+                })
+        }
     }
 })();
