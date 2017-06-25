69a638bdfb53a2131f5eb02471c5b4687939ff4f
lexylena <lexylena@gmail.com>
2017-06-25 17:24:39 -0400

    trying to access environment vars

--- project/app.js
+++ project/app.js
@@ -3,6 +3,11 @@
  */
 var app = require('../express');
 
+app.get('/hamApiKey', function(req, res) {
+    var key = process.env.HAM_API_KEY;
+    res.send(key);
+});
+
 require('./services/user.service.server');
 require('./services/collection.service.server');
 require('./services/selection.service.server');
